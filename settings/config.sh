8fae8f57fbf4a3d679def8984968d35905fa7e86
Alberto ohiaia Falchi <alberto.ohiaia.falchi@gmail.com>
2017-10-20 10:00:03 +1000

    REMOVE trash UPDATE value - URGENT

--- settings/config.sh
+++ settings/config.sh
@@ -8,10 +8,10 @@ if [[ ! $DIR ]]; then DIR="./"; fi
 
 
 # The Project name
-  export CHAOS_PROJECT="template"
+  export CHAOS_PROJECT="pluto"
 
 # Customize this to work on different Environments
-  export CHAOS_LOCAL_ENV="development"
+  export CHAOS_LOCAL_ENV="production"
 
 
 # sourcing from other files
