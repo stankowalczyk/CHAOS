cb354d8eee0abfeb2b26e8287bfcc563edc1d10d
Alberto ohiaia Falchi <alberto.ohiaia.falchi@gmail.com>
2017-10-23 09:58:04 +1000

    Run the infer_service module with the right set of variables

--- tools/run.sh
+++ tools/run.sh
@@ -6,11 +6,18 @@ run_project(){
 
   echo "Running *${CHAOS_PROJECT}* - half automated process"
 
+
+  echo "CHAOS_aws_key is *${CHAOS_aws_key}*"
+  echo "CHAOS_aws_secret is *${CHAOS_aws_secret}*"
+  echo "CHAOS_rabbit_connection_string is *${CHAOS_rabbit_connection_string}*"
+
+
+  python3 /pluto/modules/infer_service.py --aws_key ${CHAOS_aws_key} --aws_secret ${CHAOS_aws_secret} --mq_uri ${CHAOS_rabbit_connection_string}
+
   ### ### ### ### ### ### ### ### ### ### ### ###
   ## We are starting the endless loop 
   ### ### ### ### ### ### ### ### ### ### ### ###
-  tail -f /dev/null
 
-  # python3 ${DIR}/infer_service.py --aws_key ${aws_key} --aws_secret ${aws_secret} --mq_uri ${rabbit_connection_string}
+  # tail -f /dev/null
 
 }
